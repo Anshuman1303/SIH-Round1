cbdd019e652a1c36dc42e308bef17319eb2c3899
Anshuman Garg <anshuman.garg1303@gmail.com>
2024-09-01 22:27:52 +0530

    bug fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,25 @@
 import invoiceTemplate from "@/components/invoiceTemplates";
 import { defaultInvoice, loadInvoiceFromLocalStorage } from "@/utils/firestoreUtils";
 import { IInvoice } from "@/utils/types";
-import { useEffect, useState } from "react";
+import { useFocusEffect, useRouter } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
 import { ScrollView, StyleSheet } from "react-native";
 import RenderHTML from "react-native-render-html";
 
 export default function TabTwoScreen() {
   const [invoice, setInvoice] = useState<IInvoice>(defaultInvoice);
-  useEffect(() => {
-    const loadData = async () => {
-      const savedInvoice = await loadInvoiceFromLocalStorage();
-      if (savedInvoice) {
-        setInvoice(savedInvoice);
-        console.log("Invoice loaded from local storage");
-      }
-    };
-    loadData();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      const loadData = async () => {
+        const savedInvoice = await loadInvoiceFromLocalStorage();
+        if (savedInvoice) {
+          setInvoice(savedInvoice);
+          console.log("Invoice loaded from local storage");
+        }
+      };
+      loadData();
+    }, [])
+  );
   console.log("view", invoice);
   return (
     <ScrollView>
