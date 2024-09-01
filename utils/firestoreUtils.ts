6c38c86908bdefd0b62158f27f0594281f186443
Vatsal Veerwal <vatsalveerwal@gmail.com>
2024-09-01 19:17:05 +0530

    Add offline storage

--- utils/firestoreUtils.ts
+++ utils/firestoreUtils.ts
@@ -1,5 +1,6 @@
 import { db, auth } from '../firebaseConfig';
 import { collection, addDoc, query, getDocs } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { IInvoiceDocument } from './types';
 export const storeIInvoiceDocument = async (userId: string, invoice: IInvoiceDocument) => {
 	try {
@@ -10,11 +11,10 @@ export const storeIInvoiceDocument = async (userId: string, invoice: IInvoiceDoc
 	}
 };
 
-export const getIInvoiceDocuments = async (): Promise<IInvoiceDocument[]> => {
+const fetchInvoiceDocuments = async (userId: string): Promise<IInvoiceDocument[]> => {
 	try {
-		const user = auth.currentUser;
-		if (user) {
-			const userCollectionRef = collection(db, 'users', user.uid, 'IInvoiceDocument');
+		if (userId) {
+			const userCollectionRef = collection(db, 'users', userId, 'IInvoiceDocument');
 			const q = query(userCollectionRef);
 			const querySnapshot = await getDocs(q);
 
@@ -33,3 +33,37 @@ export const getIInvoiceDocuments = async (): Promise<IInvoiceDocument[]> => {
 		return [];
 	}
 };
+
+const storeInvoiceDocumentsInLocalStorage = async (invoiceDocuments: IInvoiceDocument[]) => {
+  try {
+    const jsonDocuments = JSON.stringify(invoiceDocuments);
+    await AsyncStorage.setItem('@invoiceDocuments', jsonDocuments);
+  } catch (error) {
+    console.error('Error saving invoices to local storage', error);
+  }
+};
+
+const getInvoiceDocumentsFromLocalStorage = async (): Promise<IInvoiceDocument[] | null> => {
+	try {
+	  const jsonDocuments = await AsyncStorage.getItem('@invoiceDocuments');
+	  return jsonDocuments != null ? JSON.parse(jsonDocuments) : null;
+	} catch (error) {
+	  console.error('Error retrieving invoices from local storage', error);
+	  return null;
+	}
+  };
+  
+  export const loadInvoiceDocuments = async (userId) => {
+	try {
+	  if (!userId) {
+		console.error('No user is logged in');
+		return [];
+	  }
+	  const invoiceDocuments = await fetchInvoiceDocuments(userId);
+	  await storeInvoicesInLocalStorage(invoiceDocuments);
+	  return invoiceDocuments;
+	} catch (e) {
+	  console.error('Error fetching invoices from Firebase', e);
+	  return getInvoicesFromLocalStorage();
+	}
+  };
